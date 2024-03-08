3f0ccf5b805807ff0201fc998673ebcb381cd073
mcdevittp <mcdevittp1@mymail.nku.edu>
2024-03-08 13:09:24 -0500

    completed part 2

--- Program.cs
+++ Program.cs
@@ -43,5 +43,42 @@ namespace DemoBankEvent
             BalanceAdjusted(this, e);    
         }
     }
+    class EventLister
+    {
+        private BankAccount acct;
+        public EventLister(BankAccount account) 
+        {
+            acct = account;
+            acct.BalanceAdjusted += new EventHandler(BankAccountBalanceAdjusted);
+        }
+        private void BankAccountBalanceAdjusted(Object sender, EventArgs e) 
+        {
+            WriteLine("The account balance has been adjusted");
+            WriteLine(" Account# {0} balance {1}", acct.AcctNum, acct.Balance.ToString("C2"));
+        }
+    }
+    class DemoBankAccountEvent
+    {
+        static void Main()
+        {
+            const int TRANSACTIONS = 5;
+            char code;
+            double amt;
 
+            BankAccount acct = new BankAccount(334455);
+            EventLister listener = new EventLister(acct);
+
+            for(int x = 0; x < TRANSACTIONS; ++x)
+            {
+                Write("Enter D for deposit or W for withdrawal ");
+                code = Convert.ToChar(ReadLine());
+                Write("Enter dollar amount ");
+                amt = Convert.ToDouble(ReadLine());
+                if (code == 'D')
+                    acct.MakeDeposit(amt);
+                else
+                    acct.MakeWithdrawal(amt);
+            }
+        }
+    }
 }
